d215b569e5bb772b81f69bf703558eafb5410c35
Mehmet Gerceker <mehmetg@saucelabs.com>
2015-12-08 13:39:19 -0800

    Test functional including upload, build tag option and the test names.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# Changes to be made (reference Sauce Account)
-```
-	export SAUCE_USERNAME=your_username
-	export SAUCE_ACCESS_KEY=your_access_key
-```
+![alt](https://saucelabs.com/images/sauce-labs-logo.png)
 
-# Running the tests
+## Java-TestNg-Appium-iOS
+
+>This code is presented as an example only, since your tests and testing environments may require specialized scripting. This information should be taken only as an
+>illustration of how one would set up tests with Sauce Labs, and any modifications will not be supported. For questions regarding Sauce Labs integration, please see 
+>our documentation at https://wiki.saucelabs.com/.
+
+### Setting up your Environment and Credentials
+```export SAUCE_USERNAME=<your_username>```<br>
+```export SAUCE_ACCESS_KEY=<your_access_key>```<br>
+```export API_ZIP=<path to your app zip file>```<br>
+```export BUILD_TAG=<name of your build>```<br>
+
+When using Jenkins the BUILD_TAG is set for you automatically.
+
+### Running the tests
 to run: `mvn test`
--- src/test/java/com/yourcompany/SampleSauceTest.java
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -2,6 +2,7 @@ package com.yourcompany;
 
 import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.saucerest.SauceREST;
 import com.saucelabs.testng.SauceOnDemandAuthenticationProvider;
 import com.saucelabs.testng.SauceOnDemandTestListener;
 
@@ -9,31 +10,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import static org.testng.Assert.assertEquals;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
+
+import java.io.*;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 
 /**
- * Simple TestNG test which demonstrates being instantiated via a DataProvider in order to supply multiple browser combinations.
- *
+ * Simple TestNG test which demonstrates being instantiated via a DataProvider in order to supply multiple browser
+ * combinations.
  * @author Neil Manvar
  */
 
 @Listeners({SauceOnDemandTestListener.class})
 public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnDemandAuthenticationProvider {
 
+    protected String appFile = System.getenv("APP_ZIP");
+
+    private String appURI = null;
+    private String buildName = null;
     /**
-     * Constructs a {@link com.saucelabs.common.SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
-     * supplied by environment variables or from an external file, use the no-arg {@link com.saucelabs.common.SauceOnDemandAuthentication} constructor.
+     * Constructs a {@link com.saucelabs.common.SauceOnDemandAuthentication} instance using the supplied user
+     * name/access key. To use the authentication supplied by environment variables or from an external file, use the
+     * no-arg {@link com.saucelabs.common.SauceOnDemandAuthentication} constructor.
      */
-    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(System.getenv("SAUCE_USERNAME"), System.getenv("SAUCE_ACCESS_KEY"));
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(System.getenv("SAUCE_USERNAME"),
+            System.getenv("SAUCE_ACCESS_KEY"));
 
     /**
      * ThreadLocal variable which contains the  {@link WebDriver} instance which is used to perform browser interactions with.
@@ -54,8 +67,8 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
     @DataProvider(name = "hardCodedBrowsers", parallel = true)
     public static Object[][] sauceBrowserDataProvider(Method testMethod) {
         return new Object[][]{
-            new Object[]{"iOS", "iPhone 6", "8.4", "https://s3.amazonaws.com/appium/TestApp8.4.app.zip", "", "portrait", "1.4.11"},
-            new Object[]{"iOS", "iPhone 6 Plus", "8.4", "https://s3.amazonaws.com/appium/TestApp8.4.app.zip", "", "portrait", "1.4.11"}
+            new Object[]{"iOS", "iPhone 6", "8.4", "", "portrait", "1.4.16", testMethod.getName()},
+            new Object[]{"iOS", "iPhone 6 Plus", "8.4", "", "portrait", "1.4.16", testMethod.getName()}
         };
     }
 
@@ -67,28 +80,36 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
      *
      * @param platformName Represents the platform to be run.
      * @param deviceName Represents the device to be tested on
-     * @param platform Version Represents version of the platform.
+     * @param platformVersion Version Represents version of the platform.
      * @param app Represents the location of the app under test.
-     * @return
+     * @param browserName browser name
+     * @param deviceOrientation device orientation during test.
+     * @param appiumVersion appium version to be used
+     * @return IOSDriver<WebElement> driver for the project
      * @throws MalformedURLException if an error occurs parsing the url
      */
-    private IOSDriver<WebElement> createDriver(String platformName, String deviceName, String platformVersion, String app, String browserName, String deviceOrientation, String appiumVersion) throws MalformedURLException {
+    private IOSDriver<WebElement> createDriver(String platformName, String deviceName, String platformVersion,
+                                               String browserName, String deviceOrientation, String appiumVersion, String testName) throws MalformedURLException {
 
     	DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", platformName);
         capabilities.setCapability("deviceName", deviceName);
         capabilities.setCapability("platformVersion", platformVersion);
-        capabilities.setCapability("app", app);
+        capabilities.setCapability("app", this.appURI);
         capabilities.setCapability("browserName", browserName);
         capabilities.setCapability("deviceOrientation", deviceOrientation);
         capabilities.setCapability("appiumVersion", appiumVersion);
 
-        capabilities.setCapability("name", "Java-TestNG-Appium-iOS");
+        capabilities.setCapability("name", testName);
+
+        if (this.buildName != null) {
+            capabilities.setCapability("build", this.buildName);
+        }
 
         webDriver.set(new IOSDriver<WebElement>(
-                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
-                capabilities));
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() +
+                        "@ondemand.saucelabs.com:80/wd/hub"), capabilities));
         String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
         sessionId.set(id);
         return webDriver.get();
@@ -99,20 +120,25 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
      *
      * @param platformName Represents the platform to be run.
      * @param deviceName Represents the device to be tested on
-     * @param platform Version Represents version of the platform.
+     * @param browserName browser to be used
+     * @param deviceOrientation orientation of the device in use.
      * @param app Represents the location of the app under test.
-     * @throws app if an error occurs during the running of the test
+     * @throws Exception if an error occurs during the running of the test
      */
     @Test(dataProvider = "hardCodedBrowsers")
-    public void launchTest(String platformName, String deviceName, String platformVersion, String app, String browserName, String deviceOrientation, String appiumVersion) throws Exception {
-    	IOSDriver<WebElement> driver = createDriver(platformName, deviceName, platformVersion, app, browserName, deviceOrientation, appiumVersion);
+    public void launchTest(String platformName, String deviceName, String platformVersion, String browserName,
+                           String deviceOrientation, String appiumVersion, String testName) throws Exception {
+    	IOSDriver<WebElement> driver = createDriver(platformName, deviceName, platformVersion, browserName,
+                deviceOrientation, appiumVersion, testName);
         driver.quit();
     }
 
     @Test(dataProvider = "hardCodedBrowsers")
-    public void addSumTest(String platformName, String deviceName, String platformVersion, String app, String browserName, String deviceOrientation, String appiumVersion) throws Exception {
-    	IOSDriver<WebElement> driver = createDriver(platformName, deviceName, platformVersion, app, browserName, deviceOrientation, appiumVersion);
-    	    
+    public void addSumTest(String platformName, String deviceName, String platformVersion, String browserName,
+                           String deviceOrientation, String appiumVersion, String testName) throws Exception {
+    	IOSDriver<WebElement> driver = createDriver(platformName, deviceName, platformVersion, browserName,
+                deviceOrientation, appiumVersion, testName);
+
         MobileElement fieldOne = (MobileElement) driver.findElementByAccessibilityId("TextField1");
         fieldOne.sendKeys("12");
 
@@ -152,5 +178,44 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
     public SauceOnDemandAuthentication getAuthentication() {
         return authentication;
     }
+
+    @BeforeClass
+    public void executeTestPreRun() throws Exception{
+        this.buildName = System.getenv("BUILD_TAG");
+        this.uploadAppToSauceStorage();
+
+    }
+
+    private static String getFileMD5(String filePath) throws IOException{
+        if (filePath == null || !Files.exists(Paths.get(filePath))){
+            throw new FileNotFoundException(
+                    "Please set your APP Zip file location in APP_ZIP environment variable and try again!\n"+
+                            "$ export APP_ZIP=<full path to app zip file>");
+        }
+        FileInputStream fis = new FileInputStream(new File(filePath));
+        String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis);
+        fis.close();
+        return md5;
+    }
+
+    private void uploadAppToSauceStorage() throws Exception{
+        String fileURI = "sauce-storage:%s";
+        //upload the app file to sauce storage.
+        String srcMD5 = getFileMD5(this.appFile);
+        String fileName = Paths.get(this.appFile).getFileName().toString();
+        SauceREST sauceREST = new SauceREST(this.authentication.getUsername(), this.authentication.getAccessKey());
+        String dstMD5 = sauceREST.uploadFile(new File(this.appFile));
+        if (!srcMD5.contentEquals(dstMD5)) {
+            throw new Exception("File upload failed! MD5 signatures do not match!");
+        } else {
+            this.appURI = String.format(fileURI, fileName);
+            System.out.printf("File: %s uploaded successfully!\n", this.appFile);
+        }
+    }
+
+    /*@BeforeTest
+    private void setTestName(Method method){
+        this.testName.set(method.getName());
+    }*/
 }
 
