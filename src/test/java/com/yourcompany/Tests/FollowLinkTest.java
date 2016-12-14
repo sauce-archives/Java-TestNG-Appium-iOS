e39aeb02a62dc9927731d43faa48497cfa0f925a
Titus Fortner <titusfortner@gmail.com>
2016-12-14 17:11:51 -0600

    Tests & Page Object updated

---
+++ src/test/java/com/yourcompany/Tests/FollowLinkTest.java
@@ -0,0 +1,39 @@
+package com.yourcompany.Tests;
+
+import com.yourcompany.Pages.GuineaPigPage;
+import org.openqa.selenium.InvalidElementStateException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+
+/**
+ * Created by mehmetgerceker on 12/7/15.
+ */
+
+public class FollowLinkTest extends TestBase {
+
+    /**
+     * Runs a simple test verifying link can be followed.
+     *
+     * @throws InvalidElementStateException
+     */
+    @Test(dataProvider = "hardCodedBrowsers")
+    public void verifyLinkTest(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+
+        //create webdriver session
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        GuineaPigPage page = new GuineaPigPage(driver);
+
+        page.followLink();
+
+        Assert.assertFalse(page.isOnPage());
+    }
+
+}
---
+++ src/test/java/com/yourcompany/Tests/TextInputTest.java
@@ -0,0 +1,39 @@
+package com.yourcompany.Tests;
+
+import com.yourcompany.Pages.GuineaPigPage;
+import org.openqa.selenium.InvalidElementStateException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+import java.util.UUID;
+
+
+/**
+ * Created by mehmetgerceker on 12/7/15.
+ */
+
+public class TextInputTest extends TestBase {
+
+    /**
+     * Runs a simple test verifying if the comment input is functional.
+     * @throws InvalidElementStateException
+     */
+    @org.testng.annotations.Test(dataProvider = "hardCodedBrowsers")
+    public void verifyCommentInputTest(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        String commentInputText = UUID.randomUUID().toString();
+
+        GuineaPigPage page = new GuineaPigPage(driver);
+
+        page.submitComment(commentInputText);
+
+        Assert.assertTrue(page.getSubmittedCommentText().contains(commentInputText));
+    }
+
+}
